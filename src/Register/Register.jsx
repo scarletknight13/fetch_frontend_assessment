b635e891233a867f51b27c75cd83749867edaffe
scarletknight13 <lamarbroxton@gmail.com>
2022-11-08 02:09:48 -0600

    added media queries

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,76 +1,123 @@
 import React, {useEffect, useState} from 'react'
 import './register.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function Register() {
     const [apiData, setApiData] = useState(undefined)
     const [formattedStates, setFormattedStates] = useState(undefined);
     const [formattedOccupations, setFormattedOccupations] = useState(undefined);
+    const [values, setValues] = useState({ name: '', password: '', email: '', occupation: '', state: '', confirmPassword : ''});
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+      };
+    const url = 'https://frontend-take-home.fetchrewards.com/form'; 
     useEffect(() => {
-        fetch('https://frontend-take-home.fetchrewards.com/form')
+        // get the data from list of occupations and states from api when component renders
+        fetch(url)
             .then((response) => response.json())
             .then((data) => setApiData(data));
     }, [])
     useEffect(() => {
-        console.log(apiData);
+        // console.log(apiData);
+
+        // data from api has been retrieve we can tranform data into html elements to put in our form
         if (apiData){
             setFormattedOccupations(apiData.occupations.map((occupation) => <option value={occupation}>{occupation}</option>) )
             setFormattedStates(apiData.states.map((state) => <option value={state['name']}> {state['name']}</option>));
         }
     }, [apiData])
+    // when the user presses submit button
+    function handleChange(e){
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    function validateForm(){
+        const { name, password, email, state, occupation, confirmPassword } = values;
+        console.log(password.length)
+        if (name === "" || password === "" || email === "" || state === '' || occupation === '') {
+            toast.error("All fields are required", toastOptions);
+            // console.log('here');
+            return false;
+        }
+        else if(password !== confirmPassword){
+            toast.error("passwords must match", toastOptions);
+            return false;
+        }
+        else if(password.length < 8 || password.length > 15){
+            toast.error("Password must be 8-15 characters", toastOptions);
+            return false;
+        }
+        return true;
+    }
     async function handleSubmit(e){
         e.preventDefault();
-        const values = {};
-        for(let i = 0; i < 6; ++i){
-            if(['name', 'password', 'email', 'state', 'occupation'].includes(e.target[i].name))
-                values[e.target[i].name] = e.target[i].value;
+        if(validateForm()){
+            console.log(values)
+            // console.log(values);
+    
+            // send new user object to api 
+            const response = await fetch('https://frontend-take-home.fetchrewards.com/form', {
+                method: 'POST', 
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values),
+            });
+            if(response.status === 201){
+                setValues({ name: '', password: '', email: '', occupation: '', state: '', confirmPassword : ''});
+                toast.success('Successfully Created User', toastOptions);
+            }
+            else{
+                toast.error('Error has occurred. Try again later', toastOptions);
+            }
+            console.log('response: ', response);
+
         }
-        console.log(values);
-        const response = await fetch('https://frontend-take-home.fetchrewards.com/form', {
-            method: 'POST', 
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values),
-        });
-        console.log('response: ', response);
     }
     return (
-    <form className='Register' onSubmit={(e) => handleSubmit(e)}>
+    <form className='Register' onSubmit={(e) => handleSubmit(e)} autoComplete='none'>
         <h2>Register</h2>
         <div className='name-email-container row-container'>
             <div className='name-container field-container'>
                 <label htmlFor="name">Full Name</label>
-                <input type="text" name='name' id='name'/>
+                <input value={values.name}type="text" name='name' id='name' onChange={(e) => handleChange(e)} autoComplete='none'/>
             </div>
             <div className='email-container field-container'>
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' id='email'/>
+                <input value={values.email} type="email" name='email' id='email' onChange={(e) => handleChange(e)} autoComplete={'none'}/>
             </div>
         </div>
         <div className='password-confirmpassword-container row-container'>
             <div className='password-container field-container'>
                 <label htmlFor="password">Password</label>
-                <input type="password" name='password' id='password'/>
+                <input value={values.password} type="password" name='password' id='password' onChange={(e) => handleChange(e)}/>
             </div>
             <div className='confirm-password-container field-container'>
                 <label htmlFor="confirm-password">Confirm Password</label>
-                <input type="password" name='confirm-password' id='confirm-password'/>
+                <input value={values.confirmPassword} type="password" name='confirmPassword' id='confirm-password' onChange={(e) => handleChange(e)}/>
             </div>
         </div>
         <div className='occupation-state-container row-container'>
             <div className='occupation-container field-container'>
                 <label htmlFor="occupation">Occupation</label>
-                <select name="occupation" id="occupation">
+                <select value={values.occupation} name="occupation" id="occupation" onChange={(e) => handleChange(e)}>
+                    <option value="">choose occupation</option>
                     {formattedOccupations ? formattedOccupations : ''}
                 </select>
             </div>
             <div className='state-container field-container'>
                 <label htmlFor="confirm-password">State</label>
-                <select name="state" id="state">
+                <select value={values.state} name="state" id="state" onChange={(e) => handleChange(e)}>
+                    <option value="">choose state</option>
                     {formattedStates ? formattedStates : ''}
                 </select>
             </div>
         </div>
         <button className='submitButton' type='submit' name='button'>Submit</button>
+        <ToastContainer />
     </form>
     )
 }
--- src/Register/apidata.js
+++
@@ -1,4 +0,0 @@
-let apidata;
-fetch('https://frontend-take-home.fetchrewards.com/form')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
